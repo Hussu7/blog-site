31f9e707ade3a771ce1af2fe614a8d3757f788fe
Husen Basnet <basnethusen7@gmail.com>
2024-09-22 17:01:45 +0545

    second commit

--- src/pages/blogs/Home.jsx
+++ src/pages/blogs/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Layout from '../../components/layout/Layout'
+import Card from './components/card/Card'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <Layout>
+            <div className='flex justify-center flex-wrap p-8 gap-8'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+
+
+        </Layout>
+    )
 }
 
 export default Home
